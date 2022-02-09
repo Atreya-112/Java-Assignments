1e1693dabb10e95245c543b3334277488b0a3533
Atreya-112 <atreyat17@gmail.com>
2022-02-09 17:15:54 +0530

    Modified searchByName method

--- Assignment-3/src/Search.java
+++ Assignment-3/src/Search.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 public class Search {
     public static void searchByName(ArrayList<Employee> list) throws Exception{
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter name : ");
-        String name = sc.nextLine();
+        System.out.print("Enter first name : ");
+        String firstName = sc.nextLine();
+        System.out.print("Enter last name : ");
+        String lastName = sc.nextLine();
         int flag = 0; 
         for (Employee employee : list) {
-            if (employee.getEm(0).equals(name)) {
+            if (employee.getEm(0).equals(firstName + " " + lastName)) {
                 System.out.println("Exists");
                 System.out.println(employee.toString());
                 flag = 1;
@@ -16,8 +18,9 @@ public class Search {
             }    
         }
         if (flag == 0) {
-            System.out.println("No employee with name : " + name + " exists in our record");
+            System.out.println("No employee with name : " + firstName + " " + lastName + " exists in our record.");
         }
         sc.close();
     }
+
 }
