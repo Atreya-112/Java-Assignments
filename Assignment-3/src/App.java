a3f782563a5622c2d0e66570bd04639d3ed98dba
Atreya-112 <atreyat17@gmail.com>
2022-02-09 17:17:47 +0530

    Sorting of employees by name can be performed

--- Assignment-3/src/App.java
+++ Assignment-3/src/App.java
@@ -1,8 +1,14 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 
 public class App {
     public  static void main(String[] args) throws Exception {
-        ArrayList<Employee> list = ReadEmp.read();
-        Search.searchByName(list);
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        name = name.toUpperCase();
+        System.out.println(name);
+        sc.close();
     }
 }
