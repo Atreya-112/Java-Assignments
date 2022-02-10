2a2fe10a10094f345c95d6c5946c7bf46d2e68b9
Atreya-112 <atreyat17@gmail.com>
2022-02-10 14:53:01 +0530

    Added methods for sorting

---
+++ Assignment-3/src/Sort.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Sort {
+    public static void sortByName(ArrayList<Employee> list){
+            Collections.sort(list,new UserNameComparator());
+            System.out.println(list);
+    }
+    public static void sortByNameDes(ArrayList<Employee> list){
+        Collections.sort(list,new UserNameComparator());
+        System.out.println(list);
+    }
+    public static void sortByAgeDescending(ArrayList<Employee> list){
+        Collections.sort(list,new UserAgeComparator().reversed());
+        System.out.println(list);
+    }
+    public static void sortByAge(ArrayList<Employee> list){
+        Collections.sort(list,new UserAgeComparator());
+        System.out.println(list);
+    }
+}
